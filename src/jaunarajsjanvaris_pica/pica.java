80d4788d9f86777e90071e3971069a5cec84b8cd
Daniels <d.jaunarajsjanvaris@gmail.com>
2024-04-30 12:33:12 +0300

    Tiek izveidota picas_aplikacija klase

--- src/jaunarajsjanvaris_pica/pica.java
+++ src/jaunarajsjanvaris_pica/pica.java
@@ -3,7 +3,7 @@ package jaunarajsjanvaris_pica;
 public class pica {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String izvele, vards, uzvards, adresse, picasNos; 
 
 	}
 
